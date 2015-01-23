69e4c9f5af3b189fbc2a562dfddcff895b3462e4
stevecass <steven@mumsnet.com>
2015-01-23 14:41:25 -0500

    Declare a controller in the module

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 angular.module('anguCows', ['ngResource']);
+
+angular.module('anguCows').controller('RestController', ['$scope', '$http', '$resource', 
+  function($scope, $http, $resource){
+     $scope.testData = "This is test data";
+  }
+]);
